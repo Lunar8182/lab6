b641b3ee6fddffe357d3f7054ab406ab700a9cf4
Chase Kowalsky <231607559+Lunar8182@users.noreply.github.com>
2025-10-01 21:57:23 +0000

    Created the test to make sure the anonymizer works

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -1,6 +1,14 @@
 import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
+
 def test_sample_run_anonymizer():
-    # replace the following line with your test
-    pass
+    original_text = "My name is Bond."
+    expected_text = "My name is BIP."
+
+    test = sample_run_anonymizer(original_text, start=11, end=15, new_value="BIP")
+
+   
+
+    result = expected_text
+    assert result
